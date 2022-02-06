8aca762f8c2dea9b5dfe84bbbeb8756bca329b82
Geovanny <williamnavarro@emision.co>
2022-02-06 01:45:29 -0500

    Create functions show, updated.

--- tests/Feature/app/Http/Controllers/CustomerControllerTest.php
+++ tests/Feature/app/Http/Controllers/CustomerControllerTest.php
@@ -8,6 +8,7 @@ use Tests\TestCase;
 
 class CustomerControllerTest extends TestCase
 {
+    use RefreshDatabase;
 
     public function testStoreCustomerSuccess(){
         $customer = Customer::factory()->make()->toArray();
@@ -27,5 +28,42 @@ class CustomerControllerTest extends TestCase
         $customer = Customer::factory()->create()->toArray();
         $response = $this->postJson('api/customers', $customer);
         $response->assertStatus(422);
+    }
+
+    public function testIndexCustomer(){
+        Customer::factory()->count(50)->create();
+        $response = $this->getJson('api/customers',[
+            'Accept' => 'application/json',
+            'Content-type' => 'application/json'
+        ]);
+        $response->assertOk();
+    }
+
+    public function testShowResourceCustomerSuccess(){
+        $customer = Customer::factory()->create();
+        $response = $this->getJson('api/customers/' . $customer->identification_number);
+        $response->dump();
+        $response->assertOk();
+    }
+
+    public function testShowResourceCustomerNotFound(){
+        Customer::factory()->create();
+        $response = $this->getJson('api/customers/12345678' );
+        $response->assertNotFound();
+    }
+
+    public function testUpdateResourceCustomerSuccess(){
+
+        $customer = Customer::factory()->create();
+
+        $response = $this->putJson('api/customers/' . $customer->identification_number, [
+            'phone' => '748159263',
+            'email' => 'email@email.co'
+        ]);
+        $this->assertDatabaseHas('customers', [
+            'identification_number' => $customer->identification_number,
+            'phone' => '748159263',
+            'email' => 'email@email.co'
+        ]);
     }
 }
